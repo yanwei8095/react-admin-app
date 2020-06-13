0dfd2a05d28b653496ed8676ef75f3a927b9617d
yanwei8095 <yanwei2007095@qq.com>
2020-06-14 00:39:12 +0800

    完成更新天气和时间功能

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,33 @@
 /* 
 包含n个请求函数模块
 */
-import axios from "./ajax"
+import jsonp from "jsonp";
+import axios from "./ajax";
 
 
 // 分辨开发环境还是生产环境
 const prefix = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "http://localhost:5000"
 // 请求登录函数
 export const reqLogin=(username, password) =>axios(prefix+"/login",{username,password},"post")
+
+// 请求天气函数
+export const reqWeather=(city)=>{
+	return new Promise((resolve,reject)=>{
+			jsonp(
+				`http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`,
+				(err, data) => {
+					// console.log(err, data)
+					if(!err){
+						const {dayPictureUrl,weather}= data.results[0].weather_data[0];
+						resolve({
+							weather,
+							imgUrl: dayPictureUrl
+						}); /* resolve({}),将结果对象数据传给外部.then(res=>{})中的res,*/
+					}else{
+						// 提示错误
+						reject("请求失败，网络异常~")
+						// "请求失败，网络异常~"将赋值给外部.catch((err)=>{})中的err
+					}
+				})
+	})
+}
--- src/components/header-main/index.jsx
+++ src/components/header-main/index.jsx
@@ -1,24 +1,77 @@
 import React, {Component} from 'react';
-import { Row, Col } from 'antd';
+import {withRouter} from "react-router-dom";
+import { Row, Col,Modal,message } from 'antd';
 import MyButton from "../my-button/index";
 import "./index.less";
-export default class HeaderMain extends Component {
+import {removeItem} from "../../utils/storage-utils";
+import memory from "../../utils/memory-utils";
+import dayjs from "dayjs";
+import {reqWeather} from "../../api/index.js";
+@withRouter
+class HeaderMain extends Component {
+	state={
+		sysTime:dayjs().format('YYYY-MM-DD hh:mm:ss'),
+		imgUrl: "http://api.map.baidu.com/images/weather/day/qing.png",
+		weather:"晴"
+	};
+
+	// 退出登录
+	logout=()=>{
+		Modal.confirm({
+			title:"您确认退出登录吗?",
+			onOk:()=>{
+				// 清空所有用户信息
+				removeItem();
+				memory.user={};
+				// 跳转到登录页面
+				this.props.history.replace("/login");//在回调函数中,只能使用this.props.history操作路由
+			},
+			okText:"确认",
+			cancelText:"取消"
+		})
+	};
+	componentDidMount(){
+		this.intervalId = setInterval(() => {
+				this.setState({
+					sysTime: dayjs().format('YYYY-MM-DD hh:mm:ss')
+				})
+			}, 1000);
+			// 请求天气数据
+			reqWeather("深圳")
+			.then(res=>{
+				this.setState({
+					imgUrl: res.imgUrl,
+					weather: res.weather
+				})
+			})
+			.catch(err=>{
+				message.error(err)
+			})
+	};
+	// 清除定时器
+	componentWillUnmount(){
+		clearInterval(this.intervalId)
+	}
+	;
 	render () {
+		const {sysTime,imgUrl,weather}=this.state;
 		return <div className="header-main">
 	   <Row className="header-main-top">
       <Col span={24}>
 				<span>欢迎,XXX</span>
-				<MyButton>退出</MyButton>
+				<MyButton onClick={this.logout}>退出</MyButton>
 			</Col>
     </Row>
     <Row className="header-main-bottom">
       <Col className="header-main-left" span={6}>用户管理</Col>
       <Col className="header-main-right" span={18}>
-				<span>2020-06-13</span>
-				<img src="http://api.map.baidu.com/images/weather/day/qing.png" alt="天气"/>
-				<span>晴</span>
+				<span>{sysTime}</span>
+				<img src={imgUrl} alt="天气"/>
+				<span>{weather}</span>
 			</Col>
     </Row>
 		</div>
 	}
 }
+
+export default HeaderMain;
