0fb2cb803e1b870c87b5732d5fe65777aa18281f
yanwei8095 <yanwei2007095@qq.com>
2020-06-22 22:29:06 +0800

    完成级联选择

--- src/pages/product/save-update.jsx
+++ src/pages/product/save-update.jsx
@@ -1,18 +1,21 @@
 import React,{Component} from "react";
-import {Card,Input,Form,Cascader,InputNumber,Button } from "antd";
+import {Card,Input,Form,Cascader,InputNumber,Button,message } from "antd";
 import {ArrowLeftOutlined} from "@ant-design/icons";
+import {reqGetCategories} from "../../api/index";
 import "./save-update.less";
 const {Item}=Form;
 export default class SaveUpdate extends Component{
+	state={
+		options:[]//级联选择器数据数组
+	};
 	goBack=()=>{
 		this.props.history.goBack()
 	};
 	 onChange=(value)=>{
 		console.log(value);
 	};
-	render(){
-		// 跳转Item中label占据多少列
-		const layout = {
+	// 跳转Item中label占据多少列
+		layout = {
 		labelCol: {
     xs: { span: 24 },
     sm: { span: 2},
@@ -23,52 +26,73 @@ export default class SaveUpdate extends Component{
     sm: { span: 10},
 	}
 	};
-	const options = [
-  {
-    value: 'zhejiang',
-    label: 'Zhejiang',
-    children: [
-      {
-        value: 'hangzhou',
-        label: 'Hangzhou',
-        children: [
-          {
-            value: 'xihu',
-            label: 'West Lake',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    value: 'jiangsu',
-    label: 'Jiangsu',
-    children: [
-      {
-        value: 'nanjing',
-        label: 'Nanjing',
-        children: [
-          {
-            value: 'zhonghuamen',
-            label: 'Zhong Hua Men',
-          },
-        ],
-      },
-    ],
-  },
-];
-const onFinish = values => {
-	console.log('Success:', values);
-};
+	 // 请求分类数据的方法
+	getCategories = async (parentId) => {
+		const result = await reqGetCategories(parentId);
+			// 请求成功
+			if (result.status === 0) {
+				// 判断是一级还是二级分类
+				if (parentId === '0') {
+					this.setState({
+						options:result.data.map(item=>{
+							return {
+								label:item.name,  
+								value:item._id,
+								isLeaf: false,
+							}
+						})
+					})
+				}
+			else { //二级分类
+				this.setState({
+					options:this.state.options.map(option=>{
+						if(option.value===parentId){
+							// 说明找到了要修改的父级分类,添加属性children
+							option.children = result.data.map(item => {
+								return {
+									label: item.name,
+									value: item._id,
+								}
+							});
+							// 请求成功(加载懒加载选项),去掉loading状态
+							option.loading = false;
+							option.isleaf = true;
+						}
+						return option;
+					})
+				})
+			}
+		}		
+		else{
+		message.error(result.msg)
+	}
+  }	;
+	componentDidMount(){
+		this.getCategories('0')
+	};
+	// 加载二级分类数据
+	loadData = selectedOptions => {
+		const targetOption = selectedOptions[selectedOptions.length - 1];
+		// 会显示loading状态
+		targetOption.loading = true;
+		// 请求二级分类数据
+	this.getCategories(targetOption.value);
+  };
 
-const onFinishFailed = errorInfo => {
-	console.log('Failed:', errorInfo);
-};
+	render(){
+	const {options}=this.state;
+	const onFinish = values => {
+		console.log('Success:', values);
+	};
+
+	const onFinishFailed = errorInfo => {
+		console.log('Failed:', errorInfo);
+	};
 		return (
 			< Card 
 			title={<div className="save-update-title"><ArrowLeftOutlined className="save-arrow" onClick={this.goBack}/>&nbsp;&nbsp;<span>添加商品</span></div>}
 			>
-			<Form {...layout}   onFinish={onFinish}
+			<Form {...this.layout}   onFinish={onFinish}
       onFinishFailed={onFinishFailed}>
 				<Item label="商品名称" rules={[{ required: true,whitespace: true,}]}>
 					<Input placeholder="请输入商品名称"/>
@@ -81,7 +105,13 @@ const onFinishFailed = errorInfo => {
 				xs: { span: 24 },
 				sm: { span: 5},
 			}}>
-					<Cascader options={options} onChange={this.onChange} placeholder="请选择分类"/>
+					<Cascader 
+					options={options} 
+					onChange={this.onChange} 
+					placeholder="请选择分类"
+					loadData={this.loadData}//用于动态加载选项
+					changeOnSelect //当此项为 true 时，点选每级菜单选项值都会发生变化
+					/>
 				</Item>
 				<Item label="商品价格"	wrapperCol={{
 					xs: { span: 24 },
